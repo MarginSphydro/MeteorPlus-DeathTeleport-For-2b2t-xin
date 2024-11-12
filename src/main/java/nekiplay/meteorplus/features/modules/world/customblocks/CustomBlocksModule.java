f8a9b7d15be8e566ab63cc92bca0b7a33db6e4f4
Roman Danilov <35975332+Nekiplay@users.noreply.github.com>
2024-11-12 23:43:57 +0500

    CustomBlocks now update blocks on enable

--- src/main/java/nekiplay/meteorplus/features/modules/world/customblocks/CustomBlocksModule.java
+++ src/main/java/nekiplay/meteorplus/features/modules/world/customblocks/CustomBlocksModule.java
@@ -1,4 +1,5 @@
 package nekiplay.meteorplus.features.modules.world.customblocks;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import meteordevelopment.meteorclient.MeteorClient;
@@ -14,56 +15,66 @@ import meteordevelopment.meteorclient.utils.player.PlayerUtils;
 import meteordevelopment.orbit.EventHandler;
 import nekiplay.Main;
 import nekiplay.meteorplus.MeteorPlusAddon;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import net.minecraft.item.Item;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Heightmap;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.WorldChunk;
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.stream.Collectors;
+
 public class CustomBlocksModule extends Module {
 	public CustomBlocksModule() {
 		super(Categories.World, "Custom Blocks", "Place custom blocks in world, extension for Holograms");
 	}
 	public Gson gson = new Gson();
 	public HashMap<PosData, CustomBlockData> allBlocks = new HashMap<PosData, CustomBlockData>();
+
 	private final SettingGroup settingsGroup = settings.getDefaultGroup();
+
 	@Override
 	public void onActivate() {
 		super.onActivate();
+
 		createDefault();
 		load();
+
+		for (Chunk chunk : Utils.chunks()) {
+			updateChunkData(chunk);
+		}
 	}
+
 	public final Setting<Boolean> noSetIfAir = settingsGroup.add(new BoolSetting.Builder()
 		.name("no-set-if-air")
 		.description("No set custom block if original block equals air.")
 		.defaultValue(true)
 		.build()
 	);
-	@EventHandler
-	private void onChunkData(ChunkDataEvent event) {
-		WorldChunk chunk = event.chunk();
+
+	private void updateChunkData(Chunk chunk) {
 		String dimension = PlayerUtils.getDimension().name();
 		for (int x = chunk.getPos().getStartX(); x <= chunk.getPos().getEndX(); x++) {
 			for (int z = chunk.getPos().getStartZ(); z <= chunk.getPos().getEndZ(); z++) {
 				int height = chunk.getHeightmap(Heightmap.Type.WORLD_SURFACE).get(x - chunk.getPos().getStartX(), z - chunk.getPos().getStartZ());
+
 				for (int y = mc.world.getBottomY(); y < height; y++) {
+
 					BlockPos pos = new BlockPos(x, y, z);
+
 					BlockState bs = chunk.getBlockState(pos);
 					Block original = bs.getBlock();
+
 					CustomBlockData customBlockData = allBlocks.get(new PosData(pos));
 					if (customBlockData != null) {
 						if (customBlockData.dimension.equals(dimension)) {
-							Item item = Item.byRawId(customBlockData.block_id);
-							Block block = Block.getBlockFromItem(item);
 							if (isValidBlockForReplace(original)) {
-								mc.world.setBlockState(pos, block.getDefaultState());
+								setBlock(pos, customBlockData);
 							}
 						}
 					}
@@ -71,29 +82,45 @@ public class CustomBlocksModule extends Module {
 			}
 		}
 	}
+
+	@EventHandler
+	private void onChunkData(ChunkDataEvent event) {
+		WorldChunk chunk = event.chunk();
+		updateChunkData(chunk);
+	}
+
 	private boolean isValidBlockForReplace(Block block) {
 		if (block != Blocks.AIR && noSetIfAir.get()) {
 			return true;
 		} else if (!noSetIfAir.get()) {
 			return true;
 		}
+
 		return false;
 	}
+
+	private void setBlock(BlockPos pos, CustomBlockData data) {
+		Item item = Item.byRawId(data.block_id);
+		Block block = Block.getBlockFromItem(item);
+		BlockState state = block.getDefaultState();
+
+		mc.world.setBlockState(pos, state);
+	}
+
 	@EventHandler
 	private void onBlockUpdate(BlockUpdateEvent event) {
 		String dimension = PlayerUtils.getDimension().name();
 		CustomBlockData customBlockData = allBlocks.get(new PosData(event.pos));
 		if (customBlockData != null) {
 			if (customBlockData.dimension.equals(dimension)) {
-				Item item = Item.byRawId(customBlockData.block_id);
 				Block original = event.newState.getBlock();
-				Block block = Block.getBlockFromItem(item);
 				if (isValidBlockForReplace(original)) {
-					mc.world.setBlockState(event.pos, block.getDefaultState());
+					setBlock(event.pos, customBlockData);
 				}
 			}
 		}
 	}
+
 	private void load() {
 		File dir = new File(MeteorClient.FOLDER, "customblocks");
 		if (dir.exists()) {
@@ -117,8 +144,10 @@ public class CustomBlocksModule extends Module {
 										}
 										MeteorPlusAddon.LOG.info(Main.METEOR_LOGPREFIX + " Success loaded custom block: " + file.getName());
 									}
+
 								} catch (JsonSyntaxException e) {
 									MeteorPlusAddon.LOG.error(Main.METEOR_LOGPREFIX + " Error in custom block: " + e);
+
 								}
 							} catch (IOException e) {
 								MeteorPlusAddon.LOG.error(Main.METEOR_LOGPREFIX + " Error in custom block: " + e);
@@ -129,6 +158,7 @@ public class CustomBlocksModule extends Module {
 			}
 		}
 	}
+
 	private void createDefault() {
 		File dir = new File(MeteorClient.FOLDER, "customblocks");
 		if (!dir.exists()) {
@@ -138,10 +168,12 @@ public class CustomBlocksModule extends Module {
 		File dir2 = new File(dir, world_name);
 		if (!dir2.exists()) {
 			dir2.mkdir();
+
 			CustomBlockData hologramData = new CustomBlockData(new BlockPos(0, 64, 0), 1);
 			hologramData.dimension = PlayerUtils.getDimension().name();
 			hologramData.world = world_name;
 			String json = gson.toJson(hologramData);
+
 			File file = new File(dir2.getPath(), "0.json");
 			if (!file.exists()) {
 				try {
