034eb402d0ca282f6fa21cfb07683bd6c6957d1a
Roman Danilov <35975332+Nekiplay@users.noreply.github.com>
2024-11-24 21:52:34 +0500

    Update KillAuraMixin.java

--- src/main/java/nekiplay/meteorplus/mixin/meteorclient/modules/KillAuraMixin.java
+++ src/main/java/nekiplay/meteorplus/mixin/meteorclient/modules/KillAuraMixin.java
@@ -28,6 +28,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import nekiplay.meteorplus.features.modules.movement.fly.FlyPlus;
 
 import java.util.ArrayList;
 import java.util.List;
