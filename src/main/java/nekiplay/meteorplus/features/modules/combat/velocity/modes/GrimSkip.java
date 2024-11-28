789eddf5f202b28f3616218417fabf3c71d73be6
Roman Danilov <neci.03@mail.ru>
2024-11-28 05:14:01 +0500

    Update GrimSkip.java

--- src/main/java/nekiplay/meteorplus/features/modules/combat/velocity/modes/GrimSkip.java
+++ src/main/java/nekiplay/meteorplus/features/modules/combat/velocity/modes/GrimSkip.java
@@ -5,6 +5,7 @@ import nekiplay.meteorplus.features.modules.combat.velocity.VelocityMode;
 import nekiplay.meteorplus.features.modules.combat.velocity.VelocityModes;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.network.packet.s2c.play.EntityDamageS2CPacket;
 import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
 import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket;
 
@@ -32,7 +33,9 @@ public class GrimSkip extends VelocityMode {
 	@Override
 	public void onReceivePacket(PacketEvent.Receive event) {
 		Packet<?> packet = event.packet;
-
+		if (packet instanceof EntityDamageS2CPacket && ((EntityDamageS2CPacket) packet).entityId() == mc.player.getId()) {
+			canCancel = true;
+		}
 		if (((packet instanceof EntityVelocityUpdateS2CPacket && ((EntityVelocityUpdateS2CPacket) packet).getId() == mc.player.getId()) || packet instanceof ExplosionS2CPacket) && canCancel) {
 			skip = 6;
 			event.cancel();
