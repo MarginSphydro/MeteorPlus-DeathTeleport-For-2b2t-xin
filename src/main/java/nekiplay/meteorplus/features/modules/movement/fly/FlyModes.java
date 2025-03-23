2a4be7713cf38f643b85a1af4441bbc0e737bc98
Roman Danilov <35975332+Nekiplay@users.noreply.github.com>
2025-03-23 20:21:00 +0500

    Update FlyModes.java

--- src/main/java/nekiplay/meteorplus/features/modules/movement/fly/FlyModes.java
+++ src/main/java/nekiplay/meteorplus/features/modules/movement/fly/FlyModes.java
@@ -3,8 +3,7 @@ package nekiplay.meteorplus.features.modules.movement.fly;
 public enum FlyModes {
 	Vulcan_Clip,
 	Matrix_Exploit_2,
-	Matrix_Exploit,
-	Damage_OldFag;
+	Matrix_Exploit;
 
 	@Override
 	public String toString() {
